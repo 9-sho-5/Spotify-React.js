64d8d8177344a4c3806ca1983d421a1240338c22
9-sho-5 <hosho.kusu@icloud.com>
2022-08-06 14:10:15 +0900

    create: spotify main body component

---
+++ src/components/Spotify.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div``;
+
+function Spotify() {
+  return (
+    <Container>
+        <div className="spotify__body">
+            <div className="body">
+                <div className="body__contents">
+                    
+                </div>
+            </div>
+        </div>
+    </Container>
+  )
+}
+
+export default Spotify

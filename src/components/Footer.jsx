cfca730784eee57d36c26173fcb62d2dfd790bf6
9-sho-5 <hosho.kusu@icloud.com>
2022-08-06 14:28:07 +0900

    fix: footer layout

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+    background-color: #181818;
+    height: 100%;
+`;
 
 function Footer() {
   return (
-    <div>
-      footer
-    </div>
+    <Container>
+        Footer
+    </Container>
   )
 }
 

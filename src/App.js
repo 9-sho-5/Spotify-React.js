ec001007db2d6c88244231d337c370fb2040c9e2
9-sho-5 <hosho.kusu@icloud.com>
2022-08-06 14:07:11 +0900

    fix: if statement -> login

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Login from './components/Login'
+import Spotify from './components/Spotify';
+import { reducerCases } from './utils/Constants';
+import { useStatePrivider } from './utils/StateProvider';
 
 function App() {
+
+  const [{ token }, dispatch] = useStatePrivider();
+  useEffect(() => {
+    const hash = window.location.hash;
+    if(hash) {
+      const token = hash.substring(1).split("&")[0].split("=")[1];
+      console.log(token);
+      dispatch({ type: reducerCases.SET_TOKEN, token })
+    }
+  }, [ token, dispatch ]);
+
   return (
     <div>
-      <Login />
+      {token ? <Spotify /> : <Login />}
     </div>
   )
 }

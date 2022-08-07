fb6455b93f96a8675540ac9bff38d539a8b1309a
9-sho-5 <hosho.kusu@icloud.com>
2022-08-07 14:42:42 +0900

    create: show playlist songs function

--- src/utils/reducer.js
+++ src/utils/reducer.js
@@ -4,6 +4,8 @@ export const initialState = {
     token: null,
     playlists: [],
     userInfo: null,
+    selectedPlaylistId: '37i9dQZF1DX5CHJY6ZqPPz',
+    selectedPlaylist: null,
 };
 
 const reducer = ( state, action ) => {
@@ -24,6 +26,12 @@ const reducer = ( state, action ) => {
             return {
                 ...state,
                 userInfo: action.userInfo,
+            }
+        }
+        case reducerCases.SET_PLAYLIST :{
+            return {
+                ...state,
+                selectedPlaylist: action.selectedPlaylist,
             }
         }
         default: 

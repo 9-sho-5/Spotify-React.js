30d8048eabf86202f8c46da0d8a53cc0426d677e
9-sho-5 <hosho.kusu@icloud.com>
2022-08-08 23:40:21 +0900

    create: play track function & fix: context_url -> context_uri

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -100,7 +100,7 @@ function Body({ headerBackground }) {
           image: track.album.images[2].url,
           duration: track.duration_ms,
           album: track.album.name,
-          context_url: track.album.uri,
+          context_uri: track.album.uri,
           track_number: track.track_number,
         }))
       }
@@ -112,6 +112,34 @@ function Body({ headerBackground }) {
     const minutes = Math.floor( ms / 60000 );
     const seconds = (( ms % 60000) / 1000).toFixed(0);
     return minutes + ":" + ( seconds < 10 ? "0" : "") + seconds;
+  }
+
+  const playTrack = async ( id, name, artists, image, context_uri, track_number ) => {
+    const response = await axios.put(
+      `https://api.spotify.com/v1/me/player/play`,
+      {
+        context_uri,
+        offset: {
+          position: track_number - 1,
+        },
+        position_ms: 0,
+      },
+      {
+        headers: {
+          Authorization: "Bearer " + token,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    if( response.status === 204 ){
+      const currentlyPlaying = {
+        id, name, artists, image,
+      }
+      dispatch({ type: reducerCases.SET_CUTTENTLY_PLAYING, currentlyPlaying })
+      dispatch({ type: reducerCases.SET_PLAYER_STATE, playerState: true })
+    } else {
+      dispatch({ type: reducerCases.SET_PLAYER_STATE, playerState: true })
+    }
   }
   return (
     <Container headerBackground={ headerBackground }>
@@ -158,7 +186,7 @@ function Body({ headerBackground }) {
                     track_number
                   }, index) => {
                     return (
-                      <div className="row" key={ id }>
+                      <div className="row" key={ id } onClick={() => playTrack( id, name, artists, image, context_uri, track_number )}>
                         <div className="col">
                           <span>{ index + 1 }</span>
                         </div>
